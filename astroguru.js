2e679692906aa98dd791264530664f240adf07ca
Pratik Gaurav <pratik_2002gaurav@outlook.com>
2022-06-22 21:57:28 +0530

    June-11

---
+++ astroguru.js
@@ -0,0 +1,13 @@
+function astroguru(job, location, spouse, numKids)
+{
+    console.log("You will be a " + job + " in " + location + " and married to " + spouse + " with " + numKids + " children.");
+}
+let job, location, spouse, numKids;
+
+job = "Engineer";
+location = "California";
+spouse = "Swati";
+numKids = 2;
+
+
+astroguru(job, location, spouse, numKids);
---
+++ greaterNum.js
@@ -0,0 +1,15 @@
+function greater(num1, num2)
+{
+    if(num1>num2)
+     console.log(num1+" is greater");
+    
+    else if(num1<num2)
+     console.log(num2+" is greater");
+
+    else if(num1==num2)
+     console.log("Both are euqal");
+
+    else
+     console.log("Can't Compare");
+}
+greater(5,0);
